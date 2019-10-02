bd835da776c8716df9be5a8901a3fbdd33903d43
Adriana Ochoa <15171173@itculiacan.com>
2019-10-02 09:48:47 -0600

    informacion cliente

---
+++ TroquelApi/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TroquelApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string nombre { get; set; }
+
+        public string apellido_materno { get; set; }
+
+        public string apellido_paterno { get; set; }
+
+        public string correo { get; set; }
+
+        public decimal telefono { get; set; }
+    }
+}
---
+++ TroquelApi/Models/TroquelDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace TroquelApi.Models
+{
+    public class TroquelDatabaseSettings : ITroquelDatabaseSettings
+    {
+        public string TroquelCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface ITroquelDatabaseSettings
+    {
+        string TroquelCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
