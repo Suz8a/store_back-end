819abf036ad5b0669d755d3143cc543409977787
Adriana Ochoa <15171173@itculiacan.com>
2019-10-03 09:15:53 -0600

    First commit

---
+++ TroquelApi/Models/Joya.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TroquelApi.Models
+{
+    public class Joya
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string nombre { get; set; }
+
+
+    }
+}
